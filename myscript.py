833392bd76dde80c47ecdc732502adbba356fca9
khbed <97263154+khbed@users.noreply.github.com>
2024-03-30 22:06:58 -0400

    Update myscript.py

--- myscript.py
+++ myscript.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-os.system("run python manage.py test")
+os.system("python manage.py test")
 os.system(f"git bisect start HEAD e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c")
 os.system("git bisect run python manage.py test")
 os.system("git bisect reset")
